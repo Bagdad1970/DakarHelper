ce3c88708a58de388a53a07a169bfb4cf681bc92
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-08-30 01:16:47 +0300

    Add module-info file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,25 @@
 module io.github.bagdad1970.dakarhelper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.poi;
+    requires org.json;
+    requires jxls.jexcel;
+    requires jakarta.mail;
+    requires org.apache.xmlbeans;
+    requires javafx.base;
+    requires org.apache.logging.log4j;
+    requires log4j;
 
+    exports io.github.bagdad1970.dakarhelper.datasource;
+
+    exports io.github.bagdad1970.dakarhelper.presenter;
+    opens io.github.bagdad1970.dakarhelper.presenter to javafx.fxml;
+
+    exports io.github.bagdad1970.dakarhelper.model;
+    opens io.github.bagdad1970.dakarhelper.model to javafx.fxml;
+
+    exports io.github.bagdad1970.dakarhelper.view;
+    opens io.github.bagdad1970.dakarhelper.view to javafx.fxml;
 
     opens io.github.bagdad1970.dakarhelper to javafx.fxml;
     exports io.github.bagdad1970.dakarhelper;
