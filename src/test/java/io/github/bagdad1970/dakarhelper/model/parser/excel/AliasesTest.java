38ef5092d043ce32d1bdafadd6d5ac98f7859f4e
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-08-30 01:21:49 +0300

    Create tests to Aliases class

---
+++ src/test/java/io/github/bagdad1970/dakarhelper/model/parser/excel/AliasesTest.java
@@ -0,0 +1,37 @@
+package io.github.bagdad1970.dakarhelper.model.parser.excel;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class AliasesTest {
+
+    private final Aliases aliases = new Aliases() {{
+        addAliases("name", Arrays.asList("номенклатура", "название"));
+        addAliases("price", Arrays.asList("цена", "стоимость", "опт", "оптовая", "розница", "розничная", "интернет"));
+    }};
+
+    @Test
+    public void hasAliasWithPositiveResult() {
+        Boolean result = aliases.hasAliasByKey("name", "название");
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void hasAliasWithNegativeResult() {
+        Boolean result = aliases.hasAliasByKey("price", "остаток");
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void getKeyByAlias() {
+        String result = aliases.getKeyByAlias("оптовая");
+
+        assertEquals("price", result);
+    }
+
+}
