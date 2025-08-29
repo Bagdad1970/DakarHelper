e4e7d2681ba24374029fe6ebb424b0b6f1d31c19
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-08-30 01:14:56 +0300

    Create class Config
    Config stores sensitive data

---
+++ src/main/java/io/github/bagdad1970/dakarhelper/config/Config.java
@@ -0,0 +1,25 @@
+package io.github.bagdad1970.dakarhelper.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class Config {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try (InputStream fileConfig = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(fileConfig);
+        }
+        catch (IOException exp) {
+            System.out.println(exp.getMessage());
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+}
