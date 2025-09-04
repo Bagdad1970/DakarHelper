0ae8d2e1e8c1fb316daf3c9d14795f6440f664ba
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-09-04 12:01:14 +0300

    Adding method getStorages()
    getStorages() returns storage names from parsing file

--- src/main/java/io/github/bagdad1970/dakarhelper/model/parser/excel/ExcelHeader.java
+++ src/main/java/io/github/bagdad1970/dakarhelper/model/parser/excel/ExcelHeader.java
@@ -1,18 +1,21 @@
 package io.github.bagdad1970.dakarhelper.model.parser.excel;
 
+import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.QuantityColumn;
 import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.HeaderColumn;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExcelHeader {
 
-    private List<HeaderColumn> headerColumns = new ArrayList<>();
+    private final List<HeaderColumn> headerColumns = new ArrayList<>();
 
-    public ExcelRow processRow(Row row) {
-        ExcelRow excelRow = new ExcelRow();
+    public ExcelObject processRow(Row row) {
+        ExcelObject excelObject = new ExcelObject();
 
         for (HeaderColumn headerColumn : headerColumns) {
             int columnIndex = headerColumn.getColumnIndex();
@@ -22,10 +25,23 @@ public class ExcelHeader {
 
             Object value = headerColumn.processCellValue(cell);
 
-            excelRow.addValue(key, value);
+            excelObject.addValue(key, value);
         }
 
-        return excelRow;
+        return excelObject;
+    }
+
+    public Map<String, String> getStorages() {
+        List<QuantityColumn> quantityColumns = headerColumns.stream()
+                .filter(quanity -> quanity instanceof QuantityColumn)
+                .map(quantity -> (QuantityColumn) quantity)
+                .toList();
+
+        return quantityColumns.stream()
+                .collect(Collectors.toMap(
+                        QuantityColumn::getStorageColumnName,
+                        QuantityColumn::getStorageName
+                ));
     }
 
     public void addHeaderColumn(HeaderColumn headerColumn) {
