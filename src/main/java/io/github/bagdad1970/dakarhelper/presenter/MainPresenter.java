2ee8deb3811190dc2c626e4124ba3804698f4f1c
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-09-04 12:13:27 +0300

    Presenter are binding with model and UI controller

--- src/main/java/io/github/bagdad1970/dakarhelper/presenter/MainPresenter.java
+++ src/main/java/io/github/bagdad1970/dakarhelper/presenter/MainPresenter.java
@@ -1,38 +1,42 @@
 package io.github.bagdad1970.dakarhelper.presenter;
 
-import io.github.bagdad1970.dakarhelper.App;
 import io.github.bagdad1970.dakarhelper.datasource.Company;
-import io.github.bagdad1970.dakarhelper.model.CompanyListModel;
+import io.github.bagdad1970.dakarhelper.datasource.SearchConditions;
+import io.github.bagdad1970.dakarhelper.model.CompaniesModel;
 import io.github.bagdad1970.dakarhelper.model.parser.EmailHandler;
-import io.github.bagdad1970.dakarhelper.view.CompanyListController;
-import io.github.bagdad1970.dakarhelper.view.CompanyListView;
+import io.github.bagdad1970.dakarhelper.model.parser.excel.ExcelParser;
+import io.github.bagdad1970.dakarhelper.view.CompaniesController;
+import io.github.bagdad1970.dakarhelper.view.CompaniesView;
 import io.github.bagdad1970.dakarhelper.view.MainController;
-import jakarta.mail.MessagingException;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Modality;
-import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainPresenter {
 
-    private MainController view;
-    private final CompanyListModel model = CompanyListModel.getInstance();
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final MainController view;
+    private final CompaniesModel companiesModel = CompaniesModel.getInstance();
 
     public MainPresenter(MainController view) {
         this.view = view;
     }
 
     public void openCompanyList() {
-        CompanyListView view = new CompanyListView("companyList-view.fxml", "Список компаний", Modality.WINDOW_MODAL);
+        CompaniesView view = new CompaniesView("companyList-view.fxml", "Список компаний", Modality.WINDOW_MODAL);
         view.getStage().setResizable(false);
 
         FXMLLoader loader = view.getLoader();
-        CompanyListController companiesController = loader.getController();
-        CompanyListPresenter companiesPresenter = new CompanyListPresenter(companiesController, model);
+        CompaniesController companiesController = loader.getController();
+        CompaniesPresenter companiesPresenter = new CompaniesPresenter(companiesController, companiesModel);
         companiesController.setPresenter(companiesPresenter);
 
         view.show();
@@ -40,13 +44,33 @@ public class MainPresenter {
     }
 
     public void processData() {
-        List<Company> companyList = model.getCompanies();
-        EmailHandler emailHandler = new EmailHandler("/home/bagdad/Downloads/dakarhhelper-folders", companyList);
-        emailHandler.readEmail();
+        List<Company> companyList = companiesModel.getCompanies();
+
+        Task<Void> startTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                LOGGER.info("reading email in task");
+
+                EmailHandler emailHandler = new EmailHandler("/home/bagdad/Downloads/dakarhhelper-folders", companyList);
+                emailHandler.readEmail();
+
+                Map<String, Path> companyDirs = emailHandler.getCompanyDirs();
+
+
+                SearchConditions conditions = new SearchConditions("", 0);
+                ExcelParser excelParser = new ExcelParser(companyDirs);
+
+                excelParser.parseExcelFiles(conditions);
+
+                return null;
+            }
+        };
+
+        new Thread(startTask).start();
     }
 
     public void saveCompanyList() {
-        model.saveData();
+        companiesModel.saveData();
     }
 
 }
