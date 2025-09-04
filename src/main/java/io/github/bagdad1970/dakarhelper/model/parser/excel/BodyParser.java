dbe5669134dd294cbb407ef6ec0211929b3e65bd
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-09-04 12:06:05 +0300

    Adding methods to more efficient processing files

--- src/main/java/io/github/bagdad1970/dakarhelper/model/parser/excel/BodyParser.java
+++ src/main/java/io/github/bagdad1970/dakarhelper/model/parser/excel/BodyParser.java
@@ -1,13 +1,9 @@
 package io.github.bagdad1970.dakarhelper.model.parser.excel;
 
-import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.CountColumn;
+import io.github.bagdad1970.dakarhelper.datasource.SearchConditions;
 import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.HeaderColumn;
-import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.NameColumn;
-import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.PriceColumn;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+import io.github.bagdad1970.dakarhelper.model.parser.excel.columns.QuantityColumn;
+import org.apache.poi.ss.usermodel.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,43 +13,64 @@ import java.util.stream.Collectors;
 
 public class BodyParser {
 
-    private Sheet sheet;
-    private ExcelHeader excelHeader;
-    private List<ExcelRow> items;
+    private final Sheet sheet;
+    private final ExcelHeader excelHeader;
 
     public BodyParser(Sheet sheet, ExcelHeader excelHeader) {
         this.sheet = sheet;
         this.excelHeader = excelHeader;
-        this.items = new ArrayList<>();
     }
 
-    public void parse() {
-        int maxRowIndex = excelHeader.getStartRowIndex();
-
-        for (int i = maxRowIndex + 1; i < sheet.getLastRowNum(); i++) {
+    public int getFirstValidRow() {
+        int startRowIndex = excelHeader.getStartRowIndex() + 1;
+        for (int i = startRowIndex; i < sheet.getLastRowNum(); i++) {
             Row row = sheet.getRow(i);
 
-            boolean isExcelRow = true;
-            for (HeaderColumn headerColumn : excelHeader.getHeaderColumns()) {
-                int columnIndex = headerColumn.getColumnIndex();
-
-                Cell cell = row.getCell(columnIndex);
-
-                if (cell == null || cell.getCellType() == CellType.BLANK) {
-                    isExcelRow = false;
-                    break;
-                } // проверка сложнее т.к. может быть количесттво в резерве пустым
-
+            if ( isRowValid(row) ) {
+                return i;
             }
-
-            if ( !isExcelRow ) continue;
-
-            items.add(excelHeader.processRow(row));
         }
+        return -1;
     }
 
-    public List<ExcelRow> getItems() {
-        return items;
+    private boolean isRowValid(Row row) {
+        List<HeaderColumn> headerColumns = excelHeader.getHeaderColumns();
+        Map<Class<? extends HeaderColumn>, List<HeaderColumn>> headerColumnsGroupedByClass = headerColumns.stream()
+                .collect(Collectors.groupingBy(HeaderColumn::getClass));
+
+        int countHeaderColumnValid = 0;
+        for (Class<? extends HeaderColumn> headerColumnClass : headerColumnsGroupedByClass.keySet()) {
+            List<HeaderColumn> headerColumnsByClass = headerColumnsGroupedByClass.get(headerColumnClass);
+
+            for (HeaderColumn headerColumn : headerColumnsByClass) {
+                int columnIndex = headerColumn.getColumnIndex();
+                Cell cell = row.getCell(columnIndex);
+                if ( cell != null && cell.getCellType() != CellType.BLANK && !cell.toString().contains("NULL") ) {
+                    countHeaderColumnValid++;
+                    break;
+                }
+            }
+        }
+
+        return countHeaderColumnValid == headerColumnsGroupedByClass.size();
+    }
+
+    public List<ExcelObject> parse(SearchConditions conditions) {
+        List<ExcelObject> excelObjects = new ArrayList<>();
+
+        int startRowIndex = getFirstValidRow();
+        for (int i = startRowIndex; i < sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+
+            if ( isRowValid(row) ) {
+                ExcelObject excelObject = excelHeader.processRow(row);
+
+                if (excelObject.validateConditions(conditions)) {
+                    excelObjects.add(excelObject);
+                }
+            }
+        }
+        return excelObjects;
     }
 
 }
