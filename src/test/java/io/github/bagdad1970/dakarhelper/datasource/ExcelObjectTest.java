a49f99d19b1cdfb44a462ca36000c81a25862527
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-09-09 13:12:40 +0300

    Adding test to method

--- src/test/java/io/github/bagdad1970/dakarhelper/datasource/ExcelObjectTest.java
+++ src/test/java/io/github/bagdad1970/dakarhelper/datasource/ExcelObjectTest.java
@@ -15,7 +15,7 @@ public class ExcelObjectTest {
     @Test
     public void replacePricePropsWhenHasOnlyOnePriceProp() {
         ExcelObject excelObject = new ExcelObject();
-        excelObject.addValue("price", 100.0);
+        excelObject.addProp("price", 100.0);
 
         Set<String> newPriceProps = new HashSet<>() {{
             add("internet");
@@ -25,9 +25,9 @@ public class ExcelObjectTest {
         excelObject.replacePriceProps(newPriceProps);
 
         ExcelObject expectedExcelObject = new ExcelObject();
-        expectedExcelObject.addValue("wholesale", 100.0);
-        expectedExcelObject.addValue("internet", 100.0);
-        expectedExcelObject.addValue("retail", 100.0);
+        expectedExcelObject.addProp("wholesale", 100.0);
+        expectedExcelObject.addProp("internet", 100.0);
+        expectedExcelObject.addProp("retail", 100.0);
 
         assertEquals(expectedExcelObject, excelObject);
     }
@@ -35,8 +35,8 @@ public class ExcelObjectTest {
     @Test
     public void replacePricePropsWhenHaveSeveralPriceProps() {
         ExcelObject excelObject = new ExcelObject();
-        excelObject.addValue("wholesale", 123.0);
-        excelObject.addValue("retail", 90.0);
+        excelObject.addProp("wholesale", 123.0);
+        excelObject.addProp("retail", 90.0);
 
         Set<String> newPriceProps = new HashSet<>() {{
             add("internet");
@@ -46,9 +46,9 @@ public class ExcelObjectTest {
         excelObject.replacePriceProps(newPriceProps);
 
         ExcelObject expectedExcelObject = new ExcelObject();
-        expectedExcelObject.addValue("wholesale", 123.0);
-        expectedExcelObject.addValue("internet", null);
-        expectedExcelObject.addValue("retail", 90.0);
+        expectedExcelObject.addProp("wholesale", 123.0);
+        expectedExcelObject.addProp("internet", null);
+        expectedExcelObject.addProp("retail", 90.0);
 
         assertEquals(expectedExcelObject, excelObject);
     }
@@ -56,19 +56,35 @@ public class ExcelObjectTest {
     @Test
     public void replaceQuantityProps() {
         ExcelObject excelObject = new ExcelObject();
-        excelObject.addValue("count1", 10);
+        excelObject.addProp("count1", 10);
 
-        Map<String, Integer> newQuantityProps = new HashMap<>() {{
-            put("count1", 10);
-            put("count2", 20);
+        Set<String> newQuantityProps = new HashSet<>() {{
+            add("count1");
+            add("count2");
         }};
         excelObject.replaceQuantityProps(newQuantityProps);
 
         ExcelObject expectedExcelObject = new ExcelObject();
-        expectedExcelObject.addValue("count1",10);
-        expectedExcelObject.addValue("count2", 0);
+        expectedExcelObject.addProp("count1",  10);
+        expectedExcelObject.addProp("count2", 0);
 
         assertEquals(expectedExcelObject, excelObject);
+    }
+
+    @Test
+    public void getCountsWithNullValue() {
+        ExcelObject excelObject = new ExcelObject();
+        excelObject.addProp("count1", 10);
+        excelObject.addProp("count2", null);
+
+        Map<String, Integer> counts = excelObject.getCounts();
+
+        Map<String, Integer> expectedCounts = new HashMap<>() {{
+            put("count1", 10);
+            put("count2", 0);
+        }};
+
+        assertEquals(expectedCounts, counts);
     }
 
 }
